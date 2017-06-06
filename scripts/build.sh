b6b8fdc6c23b54f0c26c1a272c2fddbdb4062f88
crossoverJie <crossoverjie@gmail.com>
2017-06-06 09:27:15 +0800

    个性化定制端口 VM启动参数

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,20 +1,22 @@
 #!/bin/sh
 
 # apollo config db info
-apollo_config_db_url=jdbc:mysql://localhost:3306/ApolloConfigDB?characterEncoding=utf8
-apollo_config_db_username=root
-apollo_config_db_password=
+apollo_config_db_url=jdbc:mysql://10.19.13.51:3306/ApolloConfigDB?characterEncoding=utf8
+apollo_config_db_username=erp_dev
+apollo_config_db_password=biuoo-erp-test@aisainfo.com
 
 # apollo portal db info
-apollo_portal_db_url=jdbc:mysql://localhost:3306/ApolloPortalDB?characterEncoding=utf8
-apollo_portal_db_username=root
-apollo_portal_db_password=
+apollo_portal_db_url=jdbc:mysql://10.19.13.51:3306/ApolloPortalDB?characterEncoding=utf8
+apollo_portal_db_username=erp_dev
+apollo_portal_db_password=biuoo-erp-test@aisainfo.com
 
 # meta server url
-dev_meta=http://localhost:8080
-fat_meta=http://localhost:8080
-uat_meta=http://localhost:8080
-pro_meta=http://localhost:8080
+dev_meta=http://10.19.13.48:8080
+# fat_meta=http://10.19.13.48:8080
+
+# 测试时候占时用不上
+# uat_meta=http://localhost:8080
+# pro_meta=http://localhost:8080
 
 META_SERVERS_OPTS="-Ddev_meta=$dev_meta -Dfat_meta=$fat_meta -Duat_meta=$uat_meta -Dpro_meta=$pro_meta"
 
